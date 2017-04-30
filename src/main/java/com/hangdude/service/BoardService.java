ba0702fba3462ad325079b8be8c68be53f2a20d0
ahamouda88 <ahmed.hamouda8888@gmail.com>
2017-04-30 11:38:21 -0400

    remove AppTest class

--- src/main/java/com/hangdude/service/BoardService.java
+++ src/main/java/com/hangdude/service/BoardService.java
@@ -1,6 +1,18 @@
 package com.hangdude.service;
 
+import java.util.List;
+
+import com.hangdude.model.Board;
+import com.hangdude.model.Category;
+import com.hangdude.model.Difficulty;
+import com.hangdude.model.GameWord;
+
 public interface BoardService {
 
-	
+	public GameWord getGameWord(Category category, Difficulty difficulty);
+
+	public List<GameWord> getAllBoards();
+
+	public Board getBoard(String email);
+
 }
